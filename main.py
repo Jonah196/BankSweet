dd4df73e865364698f6bdac395589a38c95f5f4b
WiseOldSage <danielkage4897@aol.com>
2020-04-24 18:43:34 +1200

    Added text

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ def basicWindow():
     windowExample.setWindowTitle('Basic Window Example')
     windowExample.setWindowIcon(QtGui.QIcon("img.png"))
 	
-	# Added Sione Manulea Label
+    # Added Sione Manulea Label
     labelA = QtWidgets.QLabel(windowExample)
     labelA.setText('Sione Manulea was here! :)')
     labelA.move(100, 40)
 	
+    labelB = QtWidgets.QLabel(windowExample)
+    labelB.setText('Hello. Just testing.')
+    labelB.move(200,80)
+	
     windowExample.show()
     sys.exit(app.exec_())
-
+	
 basicWindow()
