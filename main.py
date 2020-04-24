3fd35622538714720621c8028820921c15fd70aa
Sione Manulea <manulsl1@student.op.ac.nz>
2020-04-24 15:23:26 +1200

    Added Sione Manulea

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def basicWindow():
     windowExample.setGeometry(0, 0, 400, 400)
     windowExample.setWindowTitle('Basic Window Example')
     windowExample.setWindowIcon(QtGui.QIcon("img.png"))
+	
+	# Added Sione Manulea Label
+    labelA = QtWidgets.QLabel(windowExample)
+    labelA.setText('Sione Manulea was here! :)')
+    labelA.move(100, 40)
+	
     windowExample.show()
     sys.exit(app.exec_())
 
